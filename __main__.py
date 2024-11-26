c9077f0faa1c3f22d73cf70cca44899fec9f6384
VanuPhantom <tess@vanu.dev>
2024-11-26 17:10:19 +0100

    Add the selectability to the version list

--- __main__.py
+++ __main__.py
@@ -1,11 +1,22 @@
 import pytermgui as ptg
-from versions import VersionProvider, VersionList
+
+from versions import VersionList, VersionProvider
 
 version_provider = VersionProvider()
 
 with ptg.WindowManager() as manager:
+    selector_window = ptg.Window()
+
+    def on_select(version_id: str) -> None:
+        global selector_window, manager
+
+        selector_window.close()
+        manager.add(ptg.Window(version_id))
+
     manager.layout.add_slot("Body")
+
+    selector_window.set_widgets([VersionList(version_provider, on_select)])
     manager.add(
-            ptg.Window(VersionList(version_provider))
+            selector_window
     )
 
--- versions.py
+++ versions.py
@@ -1,13 +1,15 @@
-from typing import cast
+from typing import Callable, Union, cast
+
 import portablemc.standard
-from pytermgui import Button, Container, Label, Splitter, Widget, keys
-from pytermgui.widgets import keys
+from pytermgui import Button, Container, Splitter, Widget, keys
+from pytermgui import tim
 
 class VersionProvider:
     def __init__(self):
         version_manifest = portablemc.standard.VersionManifest()
         self.versions: list[str] = [version["id"] for version in version_manifest.all_versions()]
         self.page_index: int = 0
+        self.highlight_index: int = 0
 
     def get_page(self, n: int) -> list[str]:
         return self.versions[n*20:(n+1)*20]
@@ -32,16 +34,57 @@ class VersionProvider:
     def previous_page(self) -> None:
         if self.page_index > 0:
             self.page_index -= 1
+    
+    def scroll_down(self) -> None:
+        if self.highlight_index < len(self.page) - 1:
+            self.highlight_index += 1
+
+    def scroll_up(self) -> None:
+        if self.highlight_index > 0:
+            self.highlight_index -= 1
+
+class VersionListItem(Widget):
+    def __init__(self, value: str, on_click: Callable[[str], None], **args):
+        super().__init__(**args)
+        self.value = value
+        self.on_click = on_click
+        self._selected = False
+
+    @property
+    def selectables_length(self) -> int:
+        if len(self.value) > 0:
+            return 1
+        else:
+            return 0
+
+    def select(self, index: Union[int, None] = None) -> None:
+        self._selected = index is not None
+
+    def get_lines(self) -> list[str]:
+        if self._selected:
+            return [tim.parse(f"[bold]{self.value}")]
+        else:
+            return [self.value]
+
+    def handle_key(self, key: str) -> bool:
+        if key == keys.ENTER:
+            self.on_click(self.value)
+            return True
+        else:
+            return False
 
 class VersionList(Container):
-    def __init__(self, version_provider: VersionProvider) -> None:
+    def __init__(self, version_provider: VersionProvider, on_select: Callable[[str], None]) -> None:
         super().__init__()
 
         # Set up state management
         self.version_provider = version_provider
 
         # Initialise the sub-widgets
-        self.labels: list[Label] = [Label(version, parent_align=0) for version in self.version_provider.page]
+        self.labels: list[VersionListItem] = [
+                VersionListItem(version, on_select, parent_align=0)
+                for version in self.version_provider.page
+            ]
 
         previous_button: Widget = Button("<", onclick=lambda _: self.previous())
         next_button: Widget = Button(">", onclick=lambda _: self.next())
@@ -60,7 +103,9 @@ class VersionList(Container):
         for i in range(len(self.labels)):
             if i < len(page):
                 self.labels[i].value = page[i]
+                self.labels[i]._selectables_length = 1
             else:
+                self.labels[i]._selectables_length = 0
                 self.labels[i].value = ""
 
     def previous(self) -> None:
